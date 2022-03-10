0079df278b677b864db7229700e35fe8b3b82e7a
Ke Ren <rkglimmer16@gmail.com>
2022-03-10 11:49:46 -0800

    Add project files.

---
+++ UniLife-Backend-CPSC304-Proj/Controllers/PostsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UniLife_Backend_CPSC304_Proj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+
+        [HttpGet] 
+        public IEnumerable<string> Get()
+        {
+            return new[] { "My Post", "test test", "Hi fuck you" };
+        }
+    }
+}
