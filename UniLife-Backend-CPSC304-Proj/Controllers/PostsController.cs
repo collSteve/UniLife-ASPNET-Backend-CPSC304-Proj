7948e1edd32dc9ebcec3b1e36035a8aa9d71a386
Ke Ren <rkglimmer16@gmail.com>
2022-03-13 17:03:19 -0700

    Meeting changes

--- UniLife-Backend-CPSC304-Proj/Controllers/PostsController.cs
+++ UniLife-Backend-CPSC304-Proj/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace UniLife_Backend_CPSC304_Proj.Controllers
 {
+    // www.here.com/api/posts
     [Route("api/[controller]")]
     [ApiController]
     public class PostsController : ControllerBase
@@ -13,5 +14,22 @@ namespace UniLife_Backend_CPSC304_Proj.Controllers
         {
             return new[] { "My Post", "test test", "Hi fuck you" };
         }
+
+        // www.here.com/api/posts/hi
+        [Route("hi")]
+        [HttpGet]
+        public IEnumerable<int> GetHi()
+        {
+            return new[] { 1, 2, 3, 4, 5 };
+        }
+
+        // www.here.com/api/posts/hi
+        [Route("god")]
+        [HttpGet]
+        public IEnumerable<string> GetDog()
+        {
+            return new[] { "God" };
+        }
+
     }
 }
