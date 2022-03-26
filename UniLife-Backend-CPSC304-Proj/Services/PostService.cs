7be40fd1d00af5091fd852c1638a14aaf9424460
Ke Ren <rkglimmer16@gmail.com>
2022-03-25 19:53:54 -0700

    Migrated PostService to new design structure that uses QueryObjects to abstract query strings and map functions

--- UniLife-Backend-CPSC304-Proj/Services/PostService.cs
+++ UniLife-Backend-CPSC304-Proj/Services/PostService.cs
@@ -45,7 +45,8 @@ namespace UniLife_Backend_CPSC304_Proj.Services
             PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate, 
             bool asc = false)
         {
-            if (postType.ToLower().Equals(PostType.SellingPost.ToLower()))
+            #region c4
+            /*if (postType.ToLower().Equals(PostType.SellingPost.ToLower()))
             {
                 return GetAllSellingPosts(orderBy, asc);
             }
@@ -62,7 +63,10 @@ namespace UniLife_Backend_CPSC304_Proj.Services
                 throw new InvalidTypeException($"Invalid post type. Expected types: " +
                         $"<{PostType.HousingPost}>, <{PostType.SellingPost}> and <{PostType.SocialMediaPost}>." +
                         $"But received <{postType}> instead.");
-            }
+            }*/
+            #endregion
+            SelectionQueryObject<PostModel> sQuery = GetPostsByTypeQuery(postType, orderBy, asc);
+            return QueryHandler.SqlQueryFromConnection(sQuery, dbConnection);
         }
 
         private string GetOrderByAttribute(PostModel.OrderByValue order)
@@ -79,44 +83,7 @@ namespace UniLife_Backend_CPSC304_Proj.Services
                         $"But received <{order}> instead");
             }
         }
-
-        /*
-         * Throws: SqlException
-         */
-        public List<PostModel> GetAllSellingPosts(PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate,bool asc = false)
-        {
-            /*string orderAttribute = GetOrderByAttribute(orderBy);
-
-            string orderDirection = asc ? "ASC" : "DESC";
-
-            string query = @"SELECT P.pid, title, [Create_Date], [Post_Body], [Num_Likes], " +
-                                    "[Num_Dislikes], [Creator_UID], [Email], Phone_Num " +
-                                    "from [dbo].[Post] P, [dbo].[Selling_Post] SP " +
-                                    "where P.PID = SP.PID "+
-                                    $"Order By {orderAttribute} " +
-                                    $"{orderDirection}";
-            Func<DbDataReader, PostModel>  mapFunction = (x) =>
-            {
-                PostModel p = new PostModel();
-                p.Pid = (int)x[0];
-                p.Title = (string)x[1];
-                p.CreatedDate = (DateTime)x[2];
-                p.PostBody = (string)x[3];
-                p.NumLikes = (int)x[4];
-                p.NumDislikes = (int)x[5];
-                p.CreatorAid = (int)x[6];
-                p.Email = (string)x[7];
-                p.PhoneNum = Convert.ToString(x[8]);
-                return p;
-            };
-
-            return QueryHandler.SqlQueryFromConnection<PostModel>(query,
-                                        mapFunction,
-                                        dbConnection);*/
-            SelectionQueryObject<PostModel> sQuery = GetAllSellingPostsQuery(orderBy, asc);
-            return QueryHandler.SqlQueryFromConnection<PostModel>(sQuery.SqlQuery(), sQuery.QueryMapFunction, dbConnection);
-        }
-
+        
         private SelectionQueryObject<PostModel> GetAllSellingPostsQuery(PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate, bool asc = false)
         {
 
@@ -149,9 +116,133 @@ namespace UniLife_Backend_CPSC304_Proj.Services
             return sQuery;
         }
 
-        public List<PostModel> GetAllHousingPosts(PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate, bool asc = false)
+        private SelectionQueryObject<PostModel> GetAllHousingPostsQuery(PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate, bool asc = false)
         {
             string orderAttribute = GetOrderByAttribute(orderBy);
+
+            Func<DbDataReader, PostModel> mapFunction = (x) =>
+            {
+                PostModel p = new PostModel();
+                p.Pid = (int)x[0];
+                p.Title = (string)x[1];
+                p.CreatedDate = (DateTime)x[2];
+                p.PostBody = (string)x[3];
+                p.NumLikes = (int)x[4];
+                p.NumDislikes = (int)x[5];
+                p.CreatorAid = (int)x[6];
+                p.Email = (string)x[7];
+                p.Address = (string)x[8];
+                return p;
+            };
+
+            SelectionQueryObject<PostModel> sQuery = new SelectionQueryObject<PostModel>(mapFunction);
+
+            sQuery.Select("P.pid, title, [Create_Date], [Post_Body], [Num_Likes], [Num_Dislikes], [Creator_UID], [Email], [Address]")
+                .From("[dbo].[Post] P, [dbo].[Housing_Post] SP")
+                .Where("P.PID = SP.PID")
+                .OrderBy(orderAttribute)
+                .SetIsAscending(asc)
+                .SetIsDistinct(true);
+            return sQuery;
+        }
+
+        private SelectionQueryObject<PostModel> GetAllSocialMediaPostsQuery(PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate, bool asc = false)
+        {
+            string orderAttribute = GetOrderByAttribute(orderBy);
+
+            Func<DbDataReader, PostModel> mapFunction = (x) =>
+            {
+                PostModel p = new PostModel();
+                p.Pid = (int)x[0];
+                p.Title = (string)x[1];
+                p.CreatedDate = (DateTime)x[2];
+                p.PostBody = (string)x[3];
+                p.NumLikes = (int)x[4];
+                p.NumDislikes = (int)x[5];
+                p.CreatorAid = (int)x[6];
+                return p;
+            };
+
+            SelectionQueryObject<PostModel> sQuery = new SelectionQueryObject<PostModel>(mapFunction);
+
+            sQuery.Select("P.pid, title, [Create_Date], [Post_Body], [Num_Likes], [Num_Dislikes], [Creator_UID]")
+                .From("[dbo].[Post] P, [dbo].[Social_Media_Post] SP")
+                .Where("P.PID = SP.PID")
+                .OrderBy(orderAttribute)
+                .SetIsAscending(asc)
+                .SetIsDistinct(true);
+
+            return sQuery; 
+        }
+
+        public SelectionQueryObject<PostModel> GetPostsByTypeQuery(string postType,
+            PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate,
+            bool asc = false)
+        {
+            if (postType.ToLower().Equals(PostType.SellingPost.ToLower()))
+            {
+                return GetAllSellingPostsQuery(orderBy, asc);
+            }
+            else if (postType.ToLower().Equals(PostType.HousingPost.ToLower()))
+            {
+                return GetAllHousingPostsQuery(orderBy, asc);
+            }
+            else if (postType.ToLower().Equals(PostType.SocialMediaPost.ToLower()))
+            {
+                return GetAllSocialMediaPostsQuery(orderBy, asc);
+            }
+            else
+            {
+                throw new InvalidTypeException($"Invalid post type. Expected types: " +
+                        $"<{PostType.HousingPost}>, <{PostType.SellingPost}> and <{PostType.SocialMediaPost}>." +
+                        $"But received <{postType}> instead.");
+            }
+        }
+
+
+        /*
+         * Throws: SqlException
+         */
+        public List<PostModel> GetAllSellingPosts(PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate, bool asc = false)
+        {
+            #region c1
+            /*string orderAttribute = GetOrderByAttribute(orderBy);
+
+            string orderDirection = asc ? "ASC" : "DESC";
+
+            string query = @"SELECT P.pid, title, [Create_Date], [Post_Body], [Num_Likes], " +
+                                    "[Num_Dislikes], [Creator_UID], [Email], Phone_Num " +
+                                    "from [dbo].[Post] P, [dbo].[Selling_Post] SP " +
+                                    "where P.PID = SP.PID "+
+                                    $"Order By {orderAttribute} " +
+                                    $"{orderDirection}";
+            Func<DbDataReader, PostModel>  mapFunction = (x) =>
+            {
+                PostModel p = new PostModel();
+                p.Pid = (int)x[0];
+                p.Title = (string)x[1];
+                p.CreatedDate = (DateTime)x[2];
+                p.PostBody = (string)x[3];
+                p.NumLikes = (int)x[4];
+                p.NumDislikes = (int)x[5];
+                p.CreatorAid = (int)x[6];
+                p.Email = (string)x[7];
+                p.PhoneNum = Convert.ToString(x[8]);
+                return p;
+            };
+
+            return QueryHandler.SqlQueryFromConnection<PostModel>(query,
+                                        mapFunction,
+                                        dbConnection);*/
+            #endregion
+            SelectionQueryObject<PostModel> sQuery = GetAllSellingPostsQuery(orderBy, asc);
+            return QueryHandler.SqlQueryFromConnection<PostModel>(sQuery.SqlQuery(), sQuery.QueryMapFunction, dbConnection);
+        }
+
+        public List<PostModel> GetAllHousingPosts(PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate, bool asc = false)
+        {
+            #region c2
+            /*string orderAttribute = GetOrderByAttribute(orderBy);
 
             string orderDirection = asc ? "ASC" : "DESC";
 
@@ -174,16 +265,17 @@ namespace UniLife_Backend_CPSC304_Proj.Services
                 p.Email = (string)x[7];
                 p.Address = (string)x[8];
                 return p;
-            };
+            };*/
 
-            return QueryHandler.SqlQueryFromConnection<PostModel>(query,
-                                        mapFunction,
-                                        dbConnection);
+            #endregion
+            SelectionQueryObject<PostModel> sQuery = GetAllHousingPostsQuery(orderBy, asc);
+            return QueryHandler.SqlQueryFromConnection<PostModel>(sQuery, dbConnection);
         }
 
         public List<PostModel> GetAllSocialMediaPosts(PostModel.OrderByValue orderBy = PostModel.OrderByValue.CreatedDate, bool asc = false)
         {
-            string orderAttribute = GetOrderByAttribute(orderBy);
+            #region c3
+            /*string orderAttribute = GetOrderByAttribute(orderBy);
 
             string orderDirection = asc ? "ASC" : "DESC";
 
@@ -208,7 +300,10 @@ namespace UniLife_Backend_CPSC304_Proj.Services
 
             return QueryHandler.SqlQueryFromConnection<PostModel>(query,
                                         mapFunction,
-                                        dbConnection);
+                                        dbConnection);*/
+            #endregion
+            SelectionQueryObject<PostModel> sQuery = GetAllSocialMediaPostsQuery(orderBy, asc);
+            return QueryHandler.SqlQueryFromConnection<PostModel>(sQuery, dbConnection);
         }
 
         public List<PostModel> SearchPostsType(string type, 
