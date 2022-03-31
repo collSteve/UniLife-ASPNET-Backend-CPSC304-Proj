d8a13e8ddd30563c6d6fe232466ce98278f120a5
min2028 <ronaldaungkhantmin@gmail.com>
2022-03-30 22:32:50 -0700

    Advertisements Model, Controller, Service
    Added advertisements containing multiple categories query

--- UniLife-Backend-CPSC304-Proj/Services/AdvertisementService.cs
+++ UniLife-Backend-CPSC304-Proj/Services/AdvertisementService.cs
@@ -1,11 +1,11 @@
 ﻿using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
 using UniLife_Backend_CPSC304_Proj.Exceptions;
 using UniLife_Backend_CPSC304_Proj.Models;
 using UniLife_Backend_CPSC304_Proj.Utils;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace UniLife_Backend_CPSC304_Proj.Services
 {
@@ -35,6 +35,78 @@ namespace UniLife_Backend_CPSC304_Proj.Services
             catch (SqlException ex)
             {
                 throw ex;
+            }
+        }
+        
+        public List<AdvertisementModel> GetAdvertisementsWithAllCategories(
+            string[] categories,
+            AdvertisementModel.OrderByValue orderBy = AdvertisementModel.OrderByValue.CreatedDate,
+            bool asc = false)
+        {
+            SelectionQueryObject<AdvertisementModel> sQuery = GetAllAdvertisementsQuery(orderBy, asc);
+
+            string[] selectedCategoriesWhereConditions = new string[categories.Length];
+
+
+            for (int i = 0; i < categories.Length; i++)
+            {
+                selectedCategoriesWhereConditions[i] = $"C.ctg_type='{categories[i]}'";
+            }
+
+            string selectedCategoriesWhereClause = String.Join(" or ", selectedCategoriesWhereConditions);
+
+            // Division: P / selectedCategories
+            sQuery.AddToWhereClause("WHERE not exists (" +
+                $"(select C.ctg_type from Categories C where {selectedCategoriesWhereClause}) " +
+                "except " +
+                "(select AC.ctg_type from Ad_category AC where AD.AdID = AC.AdID) " +
+                ")");
+            // Make sure selected categories exist in the post_category
+            sQuery.AddToWhereClause("and 0<(select Count(C.ctg_type) from Categories C " +
+                $"where {selectedCategoriesWhereClause})");
+
+            return QueryHandler.SqlQueryFromConnection(sQuery, dbConnection);
+        }
+
+        private SelectionQueryObject<AdvertisementModel> GetAllAdvertisementsQuery(AdvertisementModel.OrderByValue orderBy = AdvertisementModel.OrderByValue.CreatedDate, bool asc = false)
+        {
+
+            string orderAttribute = GetOrderByAttribute(orderBy);
+
+            Func<DbDataReader, AdvertisementModel> mapFunction = (x) =>
+            {
+                AdvertisementModel a = new AdvertisementModel();
+                a.Adid = (int)x[0];
+                a.ad_description = (string)x[1];
+                a.price = (float)x[2];
+                a.title = (string)x[3];
+                a.clicks = (int)x[4];
+                a.CreatedDate = (DateTime)x[5];
+                return a;
+            };
+
+            SelectionQueryObject<AdvertisementModel> sQuery = new SelectionQueryObject<AdvertisementModel>(mapFunction);
+            sQuery.Select("AD.[AdID], AD.[ad_description], AD.[price], AD.[title], AD.[clicks], AD.[Create_Date]")
+                .From("[dbo].[Advertisements] AD")
+                .OrderBy(orderAttribute)
+                .SetIsAscending(asc)
+                .SetIsDistinct(true);
+
+            return sQuery;
+        }
+
+        private string GetOrderByAttribute(AdvertisementModel.OrderByValue order)
+        {
+            switch (order)
+            {
+                case AdvertisementModel.OrderByValue.Title:
+                    return "Title";
+                case AdvertisementModel.OrderByValue.CreatedDate:
+                    return "Create_Date";
+                default:
+                    throw new InvalidTypeException("Invalid Order By Value. Expecting " +
+                        $"<{AdvertisementModel.OrderByValue.Title}>, or <{AdvertisementModel.OrderByValue.CreatedDate}>. " +
+                        $"But received <{order}> instead");
             }
         }
     }
